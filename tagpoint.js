cb556bf0fc9fdf0221942e2b6680e0e77d193e51
tlincke125 <thli2739@colorado.edu>
2019-12-02 16:41:45 -0700

    modular code

---
+++ tagpoint.js
@@ -0,0 +1,7 @@
+function myFunction(request, response, dataBaseHandle) {
+    console.log(request.body);
+    // query = `INSERT INTO game (username, timestamp, tagtime) VALUES (\'${request.body.us  ername}\', \'${request.body.timestamp}'\, \'${request.body.tagtime}\');`;
+   dataBaseHandle.any(query);
+    response.send("ACK\n");
+ }
+
