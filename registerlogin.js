db94e9f357997e2d2cb65d850d0cae4e064d7c1d
ryh00 <ryho4913@gmail.com>
2019-12-02 17:03:51 -0700

    mdfd

--- registerlogin.js
+++ registerlogin.js
@@ -1,12 +1,14 @@
 
 
-function login_form(req, res, dataBaseHandle) {
+module.exports = {
+    login_form : function (req, res, dataBaseHandle) {
     query = `INSERT INTO users (email, password, score) VALUES (\'${req.body.email}\', c  rypt(\'${req.body.password}\', gen_salt(\'bf\', 8)), 0);`
     dataBaseHandle.any(query);
     console.log(req.body);
     resp.send("submited form");
-}
-
-function register_home(req, res) {
-  resp.render('login.html');
+    }
+  ,
+    register_home : function (req, res) {
+      resp.render('login.html');
+    }
 }
